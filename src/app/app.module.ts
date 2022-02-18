953435269f8bcf0bf31c9001744d474b8a69293d
GitBrainsStudio <gitbrains@gmail.com>
2022-02-18 22:17:51 +0500

    impr : import http-client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -16,7 +17,8 @@ import { ProductModule } from './features/product/product.module';
     AppRoutingModule,
     CarouselModule,
     MaterialModule,
-    ProductModule
+    ProductModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
