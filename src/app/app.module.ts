3c5436190b8518bd4266d901279eec192fe8b9da
GitBrainsStudio <gitbrains@gmail.com>
2022-02-18 22:08:06 +0500

    impr : general structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselModule } from './features/carousel/carousel.module';
+import { MaterialModule } from './features/material/material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CarouselModule } from './features/carousel/carousel.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CarouselModule
+    CarouselModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
